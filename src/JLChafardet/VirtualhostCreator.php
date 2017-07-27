2270370883a16aa25398201817255094defe7843
Jose Luis Chafardet Grimaldi <jose.chafardet@icloud.com>
2017-07-27 07:24:00 -0400

    some moar changes

---
+++ src/JLChafardet/VirtualhostCreator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace JLChafardet\VirtualhostCreator;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateVirtualHost extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('Virtualhost Creator')
+            ->setDescription('Create virtualhost files for apache.')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('testing ');
+    }
+
+}
