9ae108f6bda32732a3840a08cebd4bd066a19990
Jose Luis Chafardet Grimaldi <jose.chafardet@icloud.com>
2017-07-27 08:18:41 -0400

    changes to some files

--- src/JLChafardet/Command/CreateCommand.php
+++ src/JLChafardet/Command/CreateCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JLChafardet\VirtualhostCreator;
+namespace JLChafardet\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
