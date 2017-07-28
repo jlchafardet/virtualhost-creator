ef498e47cc12a371cdde7be30e12bc2bb600908d
Jose Luis Chafardet Grimaldi <jose.chafardet@icloud.com>
2017-07-27 21:13:41 -0400

    Logic to parse template, replace variables and look at the parsed template

--- src/JLChafardet/Command/CreateCommand.php
+++ src/JLChafardet/Command/CreateCommand.php
@@ -3,6 +3,9 @@
 namespace JLChafardet\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,9 +26,19 @@ class CreateCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /**
+         * Start measuring running time
+         *
+         */
+        $rs = getrusage();
+        function runtime($ru, $rus, $index) {
+            return ($ru["ru_$index.tv_sec"]*1000 + intval($ru["ru_$index.tv_usec"]/1000))
+                -  ($rus["ru_$index.tv_sec"]*1000 + intval($rus["ru_$index.tv_usec"]/1000));
+        }
+
+        /**
          * Lets read the configuration file
          */
-        require_once dirname(__DIR__) . "/../../conf/config.inc.php";
+        require dirname(__DIR__) . "/../../conf/config.inc.php";
 
         /**
          * get the input from the user.
@@ -42,25 +55,116 @@ class CreateCommand extends Command
         }
 
         /**
+         * Logic goes here! sucky single function crap for now.
+         */
+
+        /**
          * Styles used by the output.
          */
         $domain = new OutputFormatterStyle('green',null, array('bold'));
         $folder = new OutputFormatterStyle('yellow', null, array('bold'));
+        $warning = new OutputFormatterStyle('red',null, array('bold','blink'));
+        $bold = new OutputFormatterStyle(null, null, array('bold'));
 
         $output->getFormatter()->setStyle('domain', $domain);
         $output->getFormatter()->setStyle('folder', $folder);
-
-        $template = $this->getTemplate();
+        $output->getFormatter()->setStyle('warning', $warning);
+        $output->getFormatter()->setStyle('bold', $bold);
 
         /**
-         * Sends the message to the console.
+         * Now lets try a table and decide which is better
          */
-        $output->writeln("\nCreating Virtualhost. \nDomain is <domain>$inputDomain</domain>\nIn folder: /var/www/<folder>$inputFolder</folder>\n$ipAddress");
+        $table = new Table($output);
+        $ru = getrusage();
+        $table
+            ->setHeaders(
+                array(
+                    array(new TableCell('Results', array('colspan' => 5))),
+                    array(
+                    '<info>IP Address</>',
+                    '<info>Port</>',
+                    '<info>Domain</>',
+                    '<info>Folder</>',
+                    '<info>Status</>')))
+            ->setRows(
+                array(
+                    [
+                        '<domain>'.$ipAddress.'</>',
+                        '<domain>'.$port.'</>',
+                        '<domain>'.$inputDomain.'</>',
+                        '<folder>'.$documentRoot.'/'.$inputFolder.'</folder>',
+                        "<domain>OK</>/<warning>ERROR</warning>"
+                    ],
+                    new TableSeparator(),
+                    array(new TableCell('<info>Process completed in <warning>' . runtime($ru, $rs, "utime").'</> ms</>', array('colspan' => '5'))),
+                    array(new TableCell($this->getCredits(), array('colspan' => '5'))),
+                ))
+            ;
+
+        $table->render();
+        $output->writeln(PHP_EOL);
+        $needle = array(
+            '{IPADDRESS}',
+            '{PORT}',
+            '{DOMAIN}',
+            '{ALIAS}',
+            '{DOCUMENTROOT}',
+            '{FOLDER}'
+        );
+        $haystack = array (
+            $ipAddress,
+            $port,
+            $inputDomain,
+            $alias,
+            $documentRoot,
+            $inputFolder
+        );
+        $output->writeln(str_replace($needle, $haystack, $this->getTemplate()));
+
     }
 
     protected function getTemplate()
     {
         return file_get_contents(dirname(__DIR__) . '/../../conf/template.conf');
     }
+
+    protected function getCredits()
+    {
+        return ("<info>Thank you for using virtualhost-creator by <folder>JLChafardet.</></>");
+    }
+
+    protected function parseTemplate()
+    {
+        /**
+         * Lets read the configuration file
+         */
+        require dirname(__DIR__) . "/../../conf/config.inc.php";
+        $needle = array(
+            '{IPADDRESS}',
+            '{PORT}',
+            '{DOMAIN}',
+            '{ALIAS}',
+            '{DOCUMENTROOT}',
+            '{FOLDER}'
+        );
+        $haystack = array (
+            $ipAddress,
+            $port,
+            $inputDomain,
+            $alias,
+            $documentRoot,
+            $inputFolder
+        );
+        return str_replace($needle, $haystack, $this->getTemplate());
+    }
+
+    /**
+     * unused table outputs. (for future reference only
+     *
+     * $table->setStyle('borderless');
+     * $table->setStyle('compact');
+     *
+     * This values go just "above" the table render command.
+     */
 
 }
